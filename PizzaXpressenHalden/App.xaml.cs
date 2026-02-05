3777287a97e3c952f0b583b1cdd1856e036103d0
Abdulhadi <hadi.abdoo2003@gmail.com>
2026-02-05 18:38:26 +0100

    xmal filer

--- PizzaXpressenHalden/App.xaml.cs
+++ PizzaXpressenHalden/App.xaml.cs
@@ -1,14 +1,62 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
+using PizzaXpressenHalden.ViewModels;
 
-namespace PizzaXpressenHalden
+namespace PizzaXpressenHalden;
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    private IHost? _host;
+
+    protected override async void OnStartup(StartupEventArgs e)
     {
+        base.OnStartup(e);
+
+        _host = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                var dbPath = Path.Combine(AppContext.BaseDirectory, "PizzaXpressen.db");
+                services.AddDbContext<AppDbContext>(opt => opt.UseSqlite($"Data Source={dbPath}"));
+
+                services.AddSingleton<CsvImportService>();
+                services.AddSingleton<CustomerRetentionService>();
+                services.AddSingleton<DbInitializer>();
+                services.AddSingleton<PrintService>();
+
+                services.AddSingleton<INavigationService, NavigationService>();
+
+                services.AddSingleton<MainViewModel>();
+                services.AddTransient<OrderViewModel>();
+                services.AddTransient<PizzaRegisterViewModel>();
+                services.AddTransient<OrderLogViewModel>();
+
+                services.AddSingleton<MainWindow>();
+            })
+            .Build();
+
+        await _host.StartAsync();
+
+        var initializer = _host.Services.GetRequiredService<DbInitializer>();
+        var dataFolder = Path.Combine(AppContext.BaseDirectory, "data");
+        Directory.CreateDirectory(dataFolder);
+        await initializer.InitializeAsync(dataFolder);
+
+        var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+        mainWindow.DataContext = _host.Services.GetRequiredService<MainViewModel>();
+        mainWindow.Show();
     }
 
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        if (_host != null)
+        {
+            await _host.StopAsync();
+            _host.Dispose();
+        }
+        base.OnExit(e);
+    }
 }
