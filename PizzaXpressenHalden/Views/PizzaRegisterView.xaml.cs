5cf6b0b1d51443cea6feb346e06ef208c2ec8f35
Abdulhadi <hadi.abdoo2003@gmail.com>
2026-03-12 16:18:29 +0100

    registrering siden
    der man registrer nyte pizzaer

---
+++ PizzaXpressenHalden/Views/PizzaRegisterView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace PizzaXpressenHalden.Views;
+
+public partial class PizzaRegisterView : UserControl
+{
+    public PizzaRegisterView()
+    {
+        InitializeComponent();
+    }
+}
